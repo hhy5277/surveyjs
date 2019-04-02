b667efce1ff596772650fc892bf4d37e7119346f
Dmitry Kuzin (DevExpress) <kuzin.dmitry@corp.devexpress.com>
2019-04-02 17:14:57 +0300

    PDF first steps of traverse Survey object

--- src/jspdf/jspdfsurvey.ts
+++ src/jspdf/jspdfsurvey.ts
@@ -1,4 +1,5 @@
 import { SurveyModel } from "../survey";
+import jsPDF from "jspdf";
 
 export class JsPdfSurveyModel extends SurveyModel {
   constructor(jsonObject: any) {
@@ -6,6 +7,12 @@ export class JsPdfSurveyModel extends SurveyModel {
   }
 
   public render() {
-
+    var doc = new jsPDF();
+    var i = 0;
+    this.pages.forEach((page: any, index: any) => {
+      doc.text(new Number(i).toString(), i * 10, i * 10);
+      i++;
+    });
+    doc.save('survey_result.pdf');
   }
 }
